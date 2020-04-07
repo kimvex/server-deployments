81173a510134f6b5ddb83ccc2631675beb91c904
Benjamin De la Cruz MArtinez <chomi.numb.94@gmail.com>
2020-04-06 23:40:05 -0500

    :zap: Validators: Se agregan endpoints de validacion.

--- engine/constants.go
+++ engine/constants.go
@@ -9,6 +9,9 @@ func Con() []Paths {
 	validate := []Paths{}
 	validate = append(validate, Paths{Url: "/addhost", Method: "POST"})
 	validate = append(validate, Paths{Url: "/hosts", Method: "GET"})
-	validate = append(validate, Paths{Url: "/", Method: "GET"})
+	validate = append(validate, Paths{Url: "/connect", Method: "POST"})
+	validate = append(validate, Paths{Url: "/host/:host_id", Method: "GET"})
+	// validate = append(validate, Paths{Url: "/host/:host_id/carros/:e", Method: "GET"})
+	// validate = append(validate, Paths{Url: "/", Method: "GET"})
 	return validate
 }
